c538eb1ff9a1f882f57a57d68de123d23354faa7
Yadu Nand Babuji <yadunand@aurora-uan-0012.hostmgmt.cm.aurora.alcf.anl.gov>
2025-04-10 15:39:07 +0000

    Adding topology files

--- create_topologies.py
+++ create_topologies.py
@@ -45,30 +45,23 @@ topology = np.array(
 )
 np.savetxt("topology/topo_4.txt", topology, fmt="%d")
 
-topology = np.array(
-    [
-        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 0, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 1, 0, 1, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-    ]
-)
-np.savetxt("topology/topo_24.txt", topology, fmt="%d")
 
 
-G = nx.powerlaw_cluster_graph(24, 2, p=0.01, seed=0)
-# randomly assign edge weights (network connection probabilities)
-for u, v, w in G.edges(data=True):
-    # weight with chance of sustaining network connection
-    w["weight"] = random.choice([0.7, 0.8, 0.9, 1], p=[0.03, 0.07, 0.3, 0.6])
+
+# Powerlaw cluster graph generation for Nodes=6, 12, 24, 48, 96, 192
+
+for node in [6, 12, 24, 48, 96, 192, 384]:
+
+    G = nx.powerlaw_cluster_graph(node, 2, p=0.01, seed=0)
+    # randomly assign edge weights (network connection probabilities)
+    for u, v, w in G.edges(data=True):
+        # weight with chance of sustaining network connection
+        w["weight"] = random.choice([0.7, 0.8, 0.9, 1], p=[0.03, 0.07, 0.3, 0.6])
 
 
-topology = nx.to_numpy_array(G)
-np.savetxt("topology/topo_24.txt", topology, fmt="%d")
-print(topology)
+    topology = nx.to_numpy_array(G)
+    np.savetxt(f"topology/topo_powerlaw_{node}.txt", topology, fmt="%d")
+
+    G = nx.complete_graph(node)
+    topology = nx.to_numpy_array(G)
+    np.savetxt(f"topology/topo_full_{node}.txt", topology, fmt="%d")
